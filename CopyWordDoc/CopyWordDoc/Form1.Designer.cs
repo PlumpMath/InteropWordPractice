f1d3e96eee4c1a2e329148f1a95e2ad25095240f
Akhilesh Bhatia <akhileshbhatia93@gmail.com>
2017-01-27 10:29:14 +0530

    added initial solution

---
+++ CopyWordDoc/CopyWordDoc/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace CopyWordDoc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sourceFilePath = new System.Windows.Forms.TextBox();
+            this.browseBtn = new System.Windows.Forms.Button();
+            this.copyBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // sourceFilePath
+            // 
+            this.sourceFilePath.Location = new System.Drawing.Point(33, 26);
+            this.sourceFilePath.Name = "sourceFilePath";
+            this.sourceFilePath.Size = new System.Drawing.Size(358, 20);
+            this.sourceFilePath.TabIndex = 0;
+            // 
+            // browseBtn
+            // 
+            this.browseBtn.Location = new System.Drawing.Point(420, 26);
+            this.browseBtn.Name = "browseBtn";
+            this.browseBtn.Size = new System.Drawing.Size(75, 23);
+            this.browseBtn.TabIndex = 1;
+            this.browseBtn.Text = "Browse";
+            this.browseBtn.UseVisualStyleBackColor = true;
+            this.browseBtn.Click += new System.EventHandler(this.browseBtn_Click);
+            // 
+            // copyBtn
+            // 
+            this.copyBtn.Location = new System.Drawing.Point(104, 80);
+            this.copyBtn.Name = "copyBtn";
+            this.copyBtn.Size = new System.Drawing.Size(84, 40);
+            this.copyBtn.TabIndex = 2;
+            this.copyBtn.Text = "Copy";
+            this.copyBtn.UseVisualStyleBackColor = true;
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(316, 80);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(84, 40);
+            this.exitBtn.TabIndex = 3;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+//            this.openFileDialog1.Filter = "(*.doc)|.doc|(*.docx)|.docx";
+            this.openFileDialog1.CheckFileExists = true;
+            this.openFileDialog1.CheckPathExists = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 132);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.copyBtn);
+            this.Controls.Add(this.browseBtn);
+            this.Controls.Add(this.sourceFilePath);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox sourceFilePath;
+        private System.Windows.Forms.Button browseBtn;
+        private System.Windows.Forms.Button copyBtn;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
