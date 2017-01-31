c66c5f225f140b2d311c95cf0a97a07186d7d1ff
Akhilesh Bhatia <akhilesh_bhatia@persistent.com>
2017-01-31 19:24:43 +0530

    added para after image

--- CopyWordDoc/CopyWordDoc/Form1.cs
+++ CopyWordDoc/CopyWordDoc/Form1.cs
@@ -17,22 +17,40 @@ namespace CopyWordDoc
         //doc.SaveAs2("F:\\Akhilesh Docs\\Github Projects\\InteropWordPractice\\CopyWordDoc\\sample_output.docx");
         public Form1()
         {
-            InitializeComponent();            
+            InitializeComponent();
         }
 
         private void createBtn_Click(object sender, EventArgs e)
         {
             Word.Document doc = app.Documents.Add();
             //adding image
-            var imagePath = "F:\\Akhilesh Docs\\Github Projects\\InteropWordPractice\\CopyWordDoc\\images\\introspects.png";
-            doc.Application.ActiveDocument.Shapes.AddPicture(imagePath, Type.Missing, Type.Missing, 140, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            //var imagePath = "F:\\Akhilesh Docs\\Github Projects\\InteropWordPractice\\CopyWordDoc\\images\\introspects.png";
+            var imagePath = "D:\\Learning\\My Projects\\InteropWordPractice\\CopyWordDoc\\images\\introspects.png";
+            //doc.Application.ActiveDocument.Shapes.AddPicture(imagePath, Type.Missing, Type.Missing, 140, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            var paraImage = doc.Paragraphs.Add();
+            paraImage.Format.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+            doc.InlineShapes.AddPicture(imagePath, Range: paraImage.Range);
 
-            //adding first line. this didnt work. check insertafter method
-            doc.Application.ActiveDocument.Characters.Last.Select();
-            app.Selection.Collapse();
-            app.Selection.TypeText("Description of Aptitudes and Inclination Report");
-            doc.Application.ActiveDocument.Save();
+            //adding first line.
+            var paraText = doc.Paragraphs.Add();
+            paraText.Range.Text = "Description of Aptitudes and Inclination Report ";
+            paraText.Format.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+            Word.Range range = doc.Range(paraText.Range.Start, paraText.Range.End);
+            range.Font.Size = 16;
+            range.Font.Name = "Calibri";
+            paraText.Range.InsertParagraphAfter();
 
+            //adding table
+            var paraTable = doc.Paragraphs.Add();
+            var table = doc.Tables.Add(paraTable.Range, 4, 2);
+            object[,] array = new object[4, 2];
+            array[0, 0] = "Code:7";
+            array[0, 1] = "";
+            array[1, 0] = "Tester Tester";
+            array[1,1] = "Code:7";
+
+            https://kellyschronicles.wordpress.com/2011/08/01/write-datatable-to-an-ms-word-table-efficiently-with-c-using-a-dynamic-type/
+        http://stackoverflow.com/questions/12718044/how-to-add-items-one-at-a-time-to-to-a-new-line-a-word-document-using-word-inter
             //showing the document
             app.Visible = true;
             Application.Exit();
